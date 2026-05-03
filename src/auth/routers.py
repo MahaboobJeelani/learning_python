8a9fa0c57670321a9b7e3440587debe14b48ec56
Test <mahaboobjeelani248@gmail.com>
2026-05-03 11:02:33 +0530

    creating the refresh and access token

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -3,10 +3,16 @@ from .schemas import UserCreateModel, UserModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.db.main import get_session
 from .service import UserService
+from .schemas import UserLoginModel
+from .utils import verify_password, create_access_token
+from datetime import timedelta
+from fastapi.responses import JSONResponse
 
 
 auth_routes = APIRouter()
 user_service = UserService()
+
+REFRESH_TOKEN_EXPIRY = 2
 
 @auth_routes.post('/signup', response_model=UserModel, status_code=status.HTTP_201_CREATED)
 async def create_user(user_data:UserCreateModel, session:AsyncSession = Depends(get_session)):
@@ -16,4 +22,45 @@ async def create_user(user_data:UserCreateModel, session:AsyncSession = Depends(
     new_user = await user_service.create_user(user_data, session)
 
     return new_user
-    
+
+@auth_routes.post('/login')
+async def login_user(login_data:UserLoginModel, session:AsyncSession = Depends(get_session)):
+    email = login_data.email
+    password = login_data.password
+
+    user = await user_service.get_user_by_email(email, session) 
+
+    if user is not None:
+        password_valid = verify_password(password, user.password_hash)
+        if password_valid:
+            access_token = create_access_token(
+                user_data = {
+                    "email": user.email,
+                    "user_uid": str(user.uid)
+                }
+            )
+
+            refresh_token = create_access_token(
+                user_data = {
+                    "email": user.email,
+                    "user_uid": str(user.uid)
+                },
+                refresh = True,
+                expiry=timedelta(days=REFRESH_TOKEN_EXPIRY)
+            )
+
+            return JSONResponse(
+                content={
+                    "message": "Login Successfull",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user": {
+                        "email": user.email,
+                        "user_uid": str(user.uid)
+                    }
+                }
+            )
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail= "Invalid user or Email"
+    )
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,6 +1,11 @@
 from passlib.context import CryptContext
+from datetime import timedelta, datetime
+import jwt, uuid, logging
+from src.config import Config
 
-# posswd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+ACCESS_TOKEN_EXPIRY = 36000
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
@@ -10,4 +15,34 @@ def generate_passwd_hash(passwd: str) -> str:
 
 def verify_password(passwd: str, hash: str) -> bool:
     return pwd_context.verify(passwd, hash)
+
+def create_access_token(user_data:dict, expiry:timedelta = None, refresh:bool = False):
+    payload = {}
+    payload['user'] = user_data
+    payload['expiry'] = (datetime.now() + (expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY))).timestamp()
+
+    payload['jti'] = str(uuid.uuid4())
+    payload['refresh'] = refresh
+    token = jwt.encode(
+        payload=payload,
+        key=Config.JWT_SECRETS,
+        algorithm=Config.JWT_ALGORITHM
+    )
+
+    return token
+
+def decode_token(token:str) -> dict:
+    try:
+        token_data = jwt.decode(
+            jwt=token,
+            key=Config.JWT_SECRETS,
+            algorithms=[Config.JWT_ALGORITHM]
+        )
+
+        return token_data
+    except jwt.PyJWKError as e:
+        logging.exception(e)
+        return None
+
+
 
